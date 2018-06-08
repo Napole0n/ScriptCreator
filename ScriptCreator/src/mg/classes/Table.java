82232f7c7ecde523ad2c2f9af5cb9cf692e6e22e
Murilo Goedert <33994399+Napole0n@users.noreply.github.com>
2018-06-08 14:50:42 -0300

    Initial Load

---
+++ ScriptCreator/src/mg/classes/Table.java
@@ -0,0 +1,28 @@
+package mg.classes;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author murilo.goedert
+ */
+public class Table {
+    
+    private String tableName;
+
+    public String getTableName() {
+        return tableName;
+        
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+    
+    
+    
+}
